9be2dda519c0f08f033d27e0ad634a8b119bb5de
manuellopezhenao <68312022+manuellopezhenao@users.noreply.github.com>
2024-10-11 12:28:27 +0000

    change bcrypt for bcryptjs

--- dist/utils/encryp_data.js
+++ dist/utils/encryp_data.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.compareData = exports.encryptData = void 0;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const encryptData = async (password) => {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
